2fbed657756bbd5b375742c64948ff1a14c6cdbf
yuchi ishikawa <yuchi@yuchinobookpuro.dhcp.hakozaki.ibm.com>
2019-01-22 14:18:34 +0900

    modify train.py

--- train.py
+++ train.py
@@ -196,7 +196,7 @@ def main():
 
 
         if best_accuracy < accuracy_val[-1]:
-            best_accuracy = accuracy[-1]
+            best_accuracy = accuracy_val[-1]
             torch.save(model.state_dict(), CONFIG.result_path + '/best_accuracy_model.prm')
 
         if epoch%50 == 0 and epoch != 0:
